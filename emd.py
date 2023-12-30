9bc3d761ddb71002de5fa00b11922cf55838c813
Mohammed Terry-Jack <mohammed@wluper.com>
2023-12-30 02:14:29 +0000

    testing mechanism@

---
+++ emd.py
@@ -0,0 +1,10 @@
+from numpy import ndarray
+from ot import dist
+from math import isnan
+
+def get_score(predicted:ndarray, expected:ndarray) -> float:
+    earth_movers_distance_2d = dist(predicted, expected)
+    normalisation_factor = earth_movers_distance_2d.max()
+    earth_movers_distance_2d_normalised = earth_movers_distance_2d / normalisation_factor
+    distance = earth_movers_distance_2d_normalised.sum() / earth_movers_distance_2d_normalised.size
+    return 1. if isnan(distance) else 1-distance
---
+++ test_on_sample.py
@@ -0,0 +1,112 @@
+from matplotlib.pyplot import show, subplots
+from argparse import ArgumentParser
+from base64 import b64decode
+from json import load
+
+from matplotlib.pyplot import show, subplots
+from numpy import frombuffer, ndarray, array, ones_like, where
+
+from domain_filters.lftsf import LocalisedFourierTransformSelfFilter
+from domain_filters.contours_via_circles import detect_contours
+from domain_filters.simple import SimpleDomainFilter
+from emd import get_score 
+
+def generate_domain_pattern_from_pattern_signature(
+    width: int,
+    depth: int,
+    pattern_signature: list[str],
+) -> list[list[int]]:
+    rows = []
+    for _ in range(depth):
+        for pattern in pattern_signature:
+            rows.append(list(map(int, (pattern * width)[:width])))
+    return rows[:depth]
+
+def generate_selected_domain_patterns(
+    width: int, depth: int, pattern_signatures: list[list[str]]
+) -> list[list[int]]:
+    domain_patterns = []
+    for pattern_signature in pattern_signatures:
+        domain_patterns.append(
+            generate_domain_pattern_from_pattern_signature(
+                width=width,
+                depth=depth,
+                pattern_signature=pattern_signature,
+            )
+        )
+    return domain_patterns
+
+def fill_domains(
+    n_domains: int, segmented_image: ndarray, background_patterns: list[list[list[int]]]
+) -> ndarray:
+    filled_image = ones_like(segmented_image) * -1
+    for domain_label in range(n_domains):
+        for x, y in zip(*where(segmented_image == domain_label)):
+            background_pattern = background_patterns[domain_label]
+            filled_image[x][y] = background_pattern[x][y]
+    return filled_image
+
+
+def string_to_array(image: str, shape: tuple[int, int]) -> ndarray:
+    image_bytes = b64decode(image.encode("utf-8"))
+    image_array = frombuffer(image_bytes, dtype=int)
+    return image_array.reshape(shape).astype(bool).astype(int)
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--path", type=str, required=True)
+    parser.add_argument("--fourier_localisation_size",type=int,default=4)
+    parser.add_argument("--circles_neighbourhood_radius",type=int,default=4)
+    parser.add_argument("--circles_threshold",type=float,default=0.2)
+    parser.add_argument("--fourier_threshold",type=float, default=0.5)
+
+    arguments = parser.parse_args()
+
+    with open(arguments.path) as json_file:
+        data = load(json_file)
+    domain_pattern_signatures = [
+        domain["pattern_signature"].split("-") for domain in data["metadata"]["domains"]
+    ]
+    defects = string_to_array(
+        image=data["annotated_defects"],
+        shape=(data["metadata"]["time"], data["metadata"]["lattice_width"]),
+    ) 
+    defects = 1 - defects
+    domains = array(data["domain_regions"], dtype=int)
+    spacetime = fill_domains(
+        n_domains=len(domain_pattern_signatures),
+        segmented_image=domains,
+        background_patterns=generate_selected_domain_patterns(
+            width=data["metadata"]["lattice_width"],
+            depth=data["metadata"]["time"],
+            pattern_signatures=domain_pattern_signatures,
+        ),
+    )
+
+    simple_domain_filter = SimpleDomainFilter()
+    lftsf_domain_filter = LocalisedFourierTransformSelfFilter(
+        localisation_size=arguments.fourier_localisation_size, 
+        binarisation_threshold=arguments.fourier_threshold
+    )
+    prediction_fourier = lftsf_domain_filter.classify_spacetime(spacetime=spacetime)
+    prediction_circles = detect_contours(
+        image=spacetime, 
+        neighbourhood_radius=arguments.circles_neighbourhood_radius, 
+        threshold=arguments.circles_threshold
+    )
+    prediction_simple = simple_domain_filter.classify_spacetime(spacetime=spacetime)
+
+    score_fourier = get_score(predicted=prediction_fourier, expected=defects)
+    score_circles = get_score(predicted=prediction_circles, expected=defects)
+    score_simple = get_score(predicted=array(prediction_simple), expected=defects)
+
+    print(f"Scores:\n\tFourier={score_fourier}\n\tCircles={score_circles}\n\tSimple={score_simple}")
+
+    fig, axs = subplots(5)
+    fig.suptitle(arguments.path)
+    axs[0].imshow(spacetime, cmap="gray")
+    axs[1].imshow(defects, cmap="gray")
+    axs[2].imshow(prediction_fourier, cmap="gray")
+    axs[3].imshow(prediction_circles, cmap="gray")
+    axs[4].imshow(prediction_simple, cmap="gray")
+    show()
