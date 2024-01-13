1dbc532fcc0e19be2963c7232507d4c4d0bdcde2
Mohammed Terry-Jack <mohammed@wluper.com>
2024-01-13 01:45:21 +0000

    new metric to measure

--- metric.py
+++ metric.py
@@ -1,23 +1,26 @@
 from hilbert import decode
-from numpy import ndarray
+from numpy import ndarray, pad
 from scipy.spatial.distance import cosine
 
 
 def hilbert_flatten(matrix: ndarray, n_iterations: int = 8) -> ndarray:
     """flatten 2d matrix into 1d vector using Hilbert Curve"""
     h, w = matrix.shape
+    length = max(h, w)
+    y_pad, x_pad = length - h, length - w
+    matrix_padded = pad(
+        matrix, ((0, y_pad), (0, x_pad)), mode="constant", constant_values=0
+    )
     matrix_dimensions = 2
     coordinates = tuple(
         decode(range(h * w), matrix_dimensions, n_iterations).T.tolist()
     )
-    return matrix[coordinates]
+    return matrix_padded[coordinates]
 
 
 def get_score(predicted: ndarray, expected: ndarray) -> float:
     predicted_vector = hilbert_flatten(matrix=predicted)
     expected_vector = hilbert_flatten(matrix=expected)
 
-    score = cosine(predicted_vector, expected_vector)
-    # TODO: change this to cross-entropy-loss
-    # TODO: there is something buggy here - ensure it doesnt go outside valid coordinates
-    return score
+    distance = cosine(predicted_vector, expected_vector)
+    return 1 - distance
